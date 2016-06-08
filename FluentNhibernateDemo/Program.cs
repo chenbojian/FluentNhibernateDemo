1662b98664007d1cdce4bf6b2b5e9bf73950aa4d
Bojian Chen <bjchen@thoughtworks.com>
2016-06-08 16:34:42 +0800

    [bojian] init runable config for fluent nhibernate demo

---
+++ FluentNhibernateDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Mapping;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using NHibernate.Tool.hbm2ddl;
+
+namespace FluentNhibernateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msSqlConfiguration = MsSqlConfiguration.MsSql2008
+                .ConnectionString("Server=.;Database=cbj;Trusted_Connection=True;");
+
+            Configuration cfg = null;
+            var buildSessionFactory = Fluently.Configure()
+                .Database(msSqlConfiguration)
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Book>())
+                .ExposeConfiguration(c => cfg = c)
+                .BuildSessionFactory();
+
+            new SchemaExport(cfg).Execute(true, true, false);
+
+            var currentSession = buildSessionFactory.OpenSession();
+
+            var books = currentSession.Query<Book>()
+                .ToList();
+            currentSession.Save(new Book
+            {
+                Name = "book"
+            });
+            currentSession.Flush();
+
+            Console.Write(books);
+            Console.ReadKey();
+        }
+    }
+
+    class Book
+    {
+        public virtual long Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+
+    class BookMap : ClassMap<Book>
+    {
+        public BookMap()
+        {
+            Table("books");
+            Id(_ => _.Id).Column("id");
+            Map(_ => _.Name).Column("name");
+        }
+    }
+}
